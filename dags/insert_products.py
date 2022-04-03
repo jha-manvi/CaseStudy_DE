0cd6a2728ab5e98a2980c298bdc59a761737c9cf
Manvi Jha <manvijha@Manvis-MacBook-Air.local>
2022-04-03 21:38:13 +0200

    CSV to Postgres Table through PythonOperator

---
+++ dags/insert_products.py
@@ -0,0 +1,24 @@
+from datetime import datetime,timedelta,date
+from sqlalchemy import create_engine
+import glob
+import pandas as pd
+
+
+def insert_products():
+
+    today = date.today()
+    dd=str(today.day).zfill(2) #day  is of 2 characters
+    mm=str(today.month).zfill(2) #month is of 2 characters
+    yyyy=str(today.year)
+    type1='products'
+
+    g=glob.glob("/Users/manvijha/Documents/MyProjects/CaseStudy_DE/src_data/"+yyyy+mm+dd+"*_"+type1+".csv")[0]
+    engine = create_engine('postgresql://postgres:1234@localhost:5432/airflow_db')
+
+    df=pd.read_csv(g,sep=";")
+    df.to_sql(
+        name='products', 
+        con=engine,
+        index=False,
+        if_exists='append' # if the table already exists, append this data
+        )
